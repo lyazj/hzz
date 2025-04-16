0f1a59cf6c1a48f6a445cd01b77d957c11221244
lyazj <seeson@pku.edu.cn>
2025-04-16 15:15:13 +0200

    Update

--- powheg-madspin.py
+++ powheg-madspin.py
@@ -10,16 +10,22 @@ dsid = { }
 for result in results:
     name = result['dataset_name']
     dsid[name] = [*dsid.get(name, []), result['prepid']]
+with open('powheg-madspin-datasets.txt', 'w') as file:
+    print(*dsid, sep='\n', file=file)
 
-# https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/BTV-RunIII2024Summer24wmLHEGS-00004
+miss = open('powheg-madspin.miss', 'w')
 gridpacks = set()
 for dataset, prepid in sorted(dsid.items()):
     print(dataset, prepid)
-    res = requests.get(f'https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/{prepid[0]}')
+    # https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/BTV-RunIII2024Summer24wmLHEGS-00004
+    text = requests.get(f'https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_fragment/{prepid[0]}').text
     # '/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/ST_tch_4f_13TeV_antitop_powheg_madspin/st_tch_4f_ckm_NLO_antitop_powheg_madspin_tarball.tar.xz'
-    gridpack = re.search(r"""'/cvmfs/.*\.tar\.xz'""", res.text).group()
-    print(gridpack)
-    gridpacks.add(gridpack)
+    try:
+        gridpack = re.search(r"""/cvmfs/.*\.(?:tar\.xz|tgz)""", text).group()
+        print(gridpack)
+        gridpacks.add(gridpack)
+    except Exception:
+        print('-' * 80 + '\n', text, '-' * 80 + '\n', file=miss)
 
-with open('powheg-madspin.txt', 'w') as file:
+with open('powheg-madspin-gridpacks.txt', 'w') as file:
     print(*gridpacks, sep='\n', file=file)
