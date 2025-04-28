b82b037e0a9cc4e79d52eae204c1455c725f19e2
lyazj <seeson@pku.edu.cn>
2025-04-28 19:18:42 +0200

    Update

---
+++ polar/coffea-reweighting-test.py
@@ -0,0 +1,9 @@
+exec(open('coffea-reweighting.py').read())  # It's hard to import it...
+
+rootfiles = open('example-files.txt').read().strip().split()
+print(rootfiles)
+
+for rootfile in rootfiles:
+    events = NanoEventsFactory.from_root(f'root://cms-xrd-global.cern.ch/{rootfile}', treepath='Events').events()
+    output = ReweightProcessor().process(events)
+    break
