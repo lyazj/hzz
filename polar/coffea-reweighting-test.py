213e99adda37a35e5a0ac4f6d327140fb0376a52
lyazj <seeson@pku.edu.cn>
2025-04-29 08:44:01 +0200

    Update coffea-reweighting-test.py

--- polar/coffea-reweighting-test.py
+++ polar/coffea-reweighting-test.py
@@ -1,10 +1,18 @@
 exec(open('coffea-reweighting.py').read())  # It's hard to import it...
 
-rootfiles = open('example-files.txt').read().strip().split()
-print(rootfiles)
+import uproot
+import os
+import multiprocessing
 
-for rootfile in rootfiles:
+rootfiles = open('example-files.txt').read().strip().split()
+
+def process(rootfile):
+    print(f"Processing file: {rootfile}")
+    os.makedirs('reweighted', exist_ok=True)
+    output_filename = os.path.join('reweighted', os.path.basename(rootfile))
     events = NanoEventsFactory.from_root(f'root://cms-xrd-global.cern.ch/{rootfile}', treepath='Events').events()
     output = ReweightProcessor().process(events)
-    print(output)
-    break
+    with uproot.recreate(output_filename) as f: f['tree'] = ak.Array(output)
+    print(f"Processed {rootfile} -> {output_filename}")
+
+multiprocessing.Pool().map(process, rootfiles)
