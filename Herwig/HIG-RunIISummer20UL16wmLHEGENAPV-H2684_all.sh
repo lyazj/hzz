c761b23a25e067bb2e7eb8484b0e930e070c20c2
lyazj <seeson@pku.edu.cn>
2025-09-13 19:59:03 +0800

    Update prepid

--- Herwig/HIG-RunIISummer20UL16wmLHEGENAPV-H2684_all.sh
+++ Herwig/HIG-RunIISummer20UL16wmLHEGENAPV-H2684_all.sh
@@ -5,10 +5,10 @@
 export APPTAINER_BINDPATH='/afs,/cvmfs,/cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security,/eos,/etc/pki/ca-trust,/run/user,/var/run/user'
 
 # Use local customized fragment
-[ -s Configuration/GenProduction/python/HIG-RunIISummer20UL16wmLHEGENAPV-02684-fragment.py ] || exit $?;
+[ -s Configuration/GenProduction/python/HIG-RunIISummer20UL16wmLHEGENAPV-H2684-fragment.py ] || exit $?;
 
 # Make file executable
-chmod +x HIG-RunIISummer20UL16wmLHEGENAPV-02684_all_test.sh
+chmod +x HIG-RunIISummer20UL16wmLHEGENAPV-H2684_all_test.sh
 
 if [ -e "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/el7:amd64" ]; then
   CONTAINER_NAME="el7:amd64"
@@ -20,4 +20,4 @@ else
 fi
 # Run in singularity container
 export SINGULARITY_CACHEDIR="/tmp/$(whoami)/singularity"
-singularity run --home $PWD:$PWD /cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/$CONTAINER_NAME $(echo $(pwd)/HIG-RunIISummer20UL16wmLHEGENAPV-02684_all_test.sh) "$@"
+singularity run --home $PWD:$PWD /cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/$CONTAINER_NAME $(echo $(pwd)/HIG-RunIISummer20UL16wmLHEGENAPV-H2684_all_test.sh) "$@"
