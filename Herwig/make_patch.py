c4b16080be002f339a4d5cf420a7e534764efbe2
lyazj <seeson@pku.edu.cn>
2025-09-15 00:02:46 +0800

    Update make_patch.py

--- Herwig/make_patch.py
+++ Herwig/make_patch.py
@@ -1,21 +1,17 @@
+#!/usr/bin/env python3
+
 import sys
 import importlib
-import multiprocessing
+import subprocess
 
 INPUT = sys.argv[1]
 OUTPUT = sys.argv[1] + ".patch"
 flist = importlib.import_module(INPUT.rstrip(".py")).flist
 
-
-def is_valid(fpath):
-    import ROOT
-
-    file = ROOT.TFile.Open("root://cms-xrd-global.cern.ch/" + fpath)
-    return bool(file)
-
-
-pool = multiprocessing.Pool()
-vlist = pool.map(is_valid, flist)
-flist = [f for (f, v) in zip(flist, vlist) if v]
+xrdfs = subprocess.Popen("xrdfs cms-xrd-global.cern.ch", stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+for f in flist:
+    print("ls", f, file=xrdfs.stdin, flush=True)
+flist = xrdfs.stdout.read().strip().split()
+xrdfs.wait()
 with open(OUTPUT, "w") as patch:
     print(f"process.mixData.input.fileNames = cms.untracked.vstring({repr(flist)})", file=patch)
