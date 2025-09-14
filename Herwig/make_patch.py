1c1f5866abe00d2b38b85606b12dcd0e4b1422ba
lyazj <seeson@pku.edu.cn>
2025-09-14 18:41:10 +0200

    Patch 2018

--- Herwig/make_patch.py
+++ Herwig/make_patch.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import importlib
-import subprocess
 
 INPUT = sys.argv[1]
 OUTPUT = sys.argv[1] + ".patch"
 flist = importlib.import_module(INPUT.rstrip(".py")).flist
-
-xrdfs = subprocess.Popen("xrdfs cms-xrd-global.cern.ch", stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-for f in flist:
-    print("ls", f, file=xrdfs.stdin, flush=True)
-flist = xrdfs.stdout.read().strip().split()
-xrdfs.wait()
+prefix = flist[0]
+while True:
+    for f in flist:
+        if not f.startswith(prefix):
+            prefix = os.path.dirname(prefix)
+            break
+    else:
+        break
+with os.popen("xrdfs root://eoscms.cern.ch ls -R " + prefix) as p:
+    fset = set(p.read().strip().split())
+flist = [f for f in flist if f in fset]
 with open(OUTPUT, "w") as patch:
     print(f"process.mixData.input.fileNames = cms.untracked.vstring({repr(flist)})", file=patch)
