8a6f3bce26c06b960b930de809c410dda1e0cbd8
lyazj <seeson@pku.edu.cn>
2025-09-14 20:11:07 +0800

    Add 2017

---
+++ Herwig/HIG-RunIISummer20UL17wmLHEGEN-H0296_all_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc700
+[ -z "$1" ] && EVENTS=10 || EVENTS="$1"
+[ -z "$2" ] && UPLOAD="" || UPLOAD="/eos/user/l/legao/NanoAODStore/V0/2017/MC/GluGluHToZZTo4L_M125_TuneCH3_13TeV_powheg2_JHUGenV7011_herwig7/RunIISummer20UL17wmLHEGEN-H0296/$2.root"
+echo "Events: ${EVENTS}"
+echo "Upload: ${UPLOAD}"
+
+voms-proxy-info || exit
+
+enter () {
+  [ -r $1/src ] || scram p CMSSW $1
+  cd $1/src
+  cp -r ../../Configuration .
+  eval `scram runtime -sh`
+  scram b
+  cd ../..
+}
+
+enter CMSSW_10_6_19_patch3
+cmsDriver.py Configuration/GenProduction/python/HIG-RunIISummer20UL17wmLHEGEN-H0296-fragment.py --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN,LHE --conditions 106X_mc2017_realistic_v6 --beamspot Realistic25ns13TeVEarly2017Collision --customise_commands process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(100)" --step LHE,GEN --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIISummer20UL17wmLHEGEN-H0296_1_cfg.py --fileout file:HIG-RunIISummer20UL17wmLHEGEN-H0296.root --mc -n $EVENTS || exit $?
+
+enter CMSSW_10_6_17_patch1
+cmsDriver.py  --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions 106X_mc2017_realistic_v6 --beamspot Realistic25ns13TeVEarly2017Collision --step SIM --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIISummer20UL17SIM-00599_1_cfg.py --fileout file:HIG-RunIISummer20UL17SIM-00599.root --filein file:HIG-RunIISummer20UL17wmLHEGEN-H0296.root --runUnscheduled --mc -n $EVENTS || exit $?
+
+enter CMSSW_10_6_17_patch1
+cmsDriver.py  --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-DIGI --conditions 106X_mc2017_realistic_v6 --step DIGI,DATAMIX,L1,DIGI2RAW --procModifiers premix_stage2 --geometry DB:Extended --datamix PreMix --era Run2_2017 --python_filename HIG-RunIISummer20UL17DIGIPremix-00599_1_cfg.py --fileout file:HIG-RunIISummer20UL17DIGIPremix-00599.root --filein file:HIG-RunIISummer20UL17SIM-00599.root --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer20ULPrePremix-UL17_106X_mc2017_realistic_v6-v3/PREMIX" --runUnscheduled --mc -n $EVENTS --no_exec || exit $?
+cmsRun HIG-RunIISummer20UL17DIGIPremix-00599_1_cfg.py || exit $?
+
+enter CMSSW_9_4_14_UL_patch1
+cmsDriver.py  --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v15 --customise_commands 'process.source.bypassVersionCheck = cms.untracked.bool(True)' --step HLT:2e34v40 --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIISummer20UL17HLT-00599_1_cfg.py --fileout file:HIG-RunIISummer20UL17HLT-00599.root --filein file:HIG-RunIISummer20UL17DIGIPremix-00599.root --mc -n $EVENTS || exit $?
+
+enter CMSSW_10_6_17_patch1
+cmsDriver.py  --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 106X_mc2017_realistic_v6 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIISummer20UL17RECO-00599_1_cfg.py --fileout file:HIG-RunIISummer20UL17RECO-00599.root --filein file:HIG-RunIISummer20UL17HLT-00599.root --runUnscheduled --mc -n $EVENTS || exit $?
+
+enter CMSSW_10_6_20
+cmsDriver.py  --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 106X_mc2017_realistic_v9 --step PAT --procModifiers run2_miniAOD_UL --geometry DB:Extended --era Run2_2017 --python_filename HIG-RunIISummer20UL17MiniAODv2-00675_1_cfg.py --fileout file:HIG-RunIISummer20UL17MiniAODv2-00675.root --filein file:HIG-RunIISummer20UL17RECO-00599.root --runUnscheduled --mc -n $EVENTS || exit $?
+
+enter CMSSW_13_2_2
+cmsDriver.py  --eventcontent NANOAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 106X_mc2017_realistic_v9 --step NANO --era Run2_2017,run2_nanoAOD_106Xv2 --python_filename HIG-RunIISummer20UL17NanoAODv9-00641_1_cfg.py --fileout file:HIG-RunIISummer20UL17NanoAODv9-00641.root --filein file:HIG-RunIISummer20UL17MiniAODv2-00675.root --mc -n $EVENTS || exit $?
+
+[ ! -z "${UPLOAD}" ] && xrdcp -f HIG-RunIISummer20UL17NanoAODv9-00641.root "root://eosuser.cern.ch/${UPLOAD}"
--- Herwig/org/download.sh
+++ Herwig/org/download.sh
@@ -14,4 +14,11 @@ wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-
 wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL16RECO-00498 -O 5-HIG-RunIISummer20UL16RECO-00498.sh &
 wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL16MiniAODv2-00490 -O 6-HIG-RunIISummer20UL16MiniAODv2-00490.sh &
 wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL16NanoAODv9-00528 -O 7-HIG-RunIISummer20UL16NanoAODv9-00528.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17wmLHEGEN-00296 -O 1-HIG-RunIISummer20UL17wmLHEGEN-00296.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17SIM-00599 -O 2-HIG-RunIISummer20UL17SIM-00599.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17DIGIPremix-00599 -O 3-HIG-RunIISummer20UL17DIGIPremix-00599.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17HLT-00599 -O 4-HIG-RunIISummer20UL17HLT-00599.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17RECO-00599 -O 5-HIG-RunIISummer20UL17RECO-00599.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17MiniAODv2-00675 -O 6-HIG-RunIISummer20UL17MiniAODv2-00675.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL17NanoAODv9-00641 -O 7-HIG-RunIISummer20UL17NanoAODv9-00641.sh &
 wait
